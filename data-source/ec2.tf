f5e3f44b3b358ac576dcdca1d1252c1d0dfd480b
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-10-21 17:19:56 +0530

    first commit

---
+++ data-source/ec2.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "example" {
+    ami = data.aws_ami.joindevops.id
+    instance_type = "t3.micro"
+    vpc_security_group_ids=[aws_security_group.allow-terraform.id]
+
+    tags = {
+        Name = "HelloWorld"
+        terraform = "true"
+   }
+}
+
+
+resource "aws_security_group" "allow-terraform" {
+  name   = "sg"
+  
+
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+    ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
