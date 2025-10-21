6ebd741c930efe9aae83797f54e0ade4699213f7
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-10-21 20:52:39 +0530

    state-file

---
+++ Remote-state/ec2.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "example" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids=[aws_security_group.allow-terraform.id]
+
+    tags = {
+        Name = "HelloWorld"
+        terraform = "true"
+   }
+}
+
+
+resource "aws_security_group" "allow-terraform" {
+  name   = "sg"
+  
+
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+    ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
